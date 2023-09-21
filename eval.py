f117e305d176b812a341490464a461b78a88395f
tijmenbee <75034218+tijmenbee@users.noreply.github.com>
2023-09-21 14:38:00 +0200

    Added different evaluation methods
    Standard is a weighted F1 but with a little tweaking it is also possible to show precision, recall and a non weighted F1

--- eval.py
+++ eval.py
@@ -15,9 +15,10 @@ else:
 print()
 
 
+
 def test_model_accuracy(model, model_name: str, deduped=False):
     from data import test_data, deduped_test_data
-
+    from collections import Counter
     if TRAINING:
         testing_data = dev_data if not deduped else deduped_dev_data
     else:
@@ -28,15 +29,55 @@ def test_model_accuracy(model, model_name: str, deduped=False):
 
     pred_acts = model.predict(test_sentences)
 
+    FP = []
+    TP = []
+    FN = []
+  
+    acts = Counter(test_acts)
+    for i in range(len(test_acts)):
+        if pred_acts[i] == test_acts[i]:
+            TP.append(test_acts[i])
+        else:
+            FP.append(test_acts[i])
+            FN.append(pred_acts[i])
+    
+    TP = Counter(TP)
+    FN = Counter(FN)
+    FP = Counter(FP)
+    
     correct = sum(pred_act == test_act for pred_act, test_act in zip(pred_acts, test_acts))
 
-    print(f"{model_name} accuracy: {correct / len(testing_data) * 100:.1f}% ({model.info})")
+    F1_dict = {}
+    for act in list(acts):
+        if TP[act] == 0:
+            precision = 0
+            recall = 0
+            F1 = 0
+        else:
+            precision = TP[act]/(TP[act]+FP[act])
+            recall = TP[act]/(TP[act]+FN[act])
+            F1 = 2*(precision*recall)/(precision+recall)
+            accuracy = TP[act]/(TP[act]+FN[act]+FP[act])
+        #print(act, ", precision: ", precision,  ", recall: ", recall,  ", F1: ", F1,  ", accuracy: ", accuracy)
+        F1_dict[act] = F1
+    weighted_f1 = sum(F1_dict[key]*acts.get(key, 0) for key in acts) / len(test_acts)
+
+    print(f"{model_name} accuracy: {correct / len(testing_data) * 100:.1f}% , weighted F1: {weighted_f1} ({model.info})")
 
     del test_data  # No leakage!
     del deduped_test_data
 
 
+
+def test_model_precision(model, model_name: str, deduped=False):
+    from data import test_data, deduped_test_data
+    if TRAINING:
+        testing_data = dev_data if not deduped else deduped_dev_data
+    else:
+        testing_data = test_data if not deduped else deduped_test_data
+
 # TODO evaluate on precision, recall, f1?
+
 
 
 train_acts = [act for act, _ in train_data]
