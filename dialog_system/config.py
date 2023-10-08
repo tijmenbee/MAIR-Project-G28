cf13d11ad4dd0a1dabb9399e1ede151c6cba7e9e
Giorgio Momigliano <gmomigliano@protonmail.com>
2023-10-08 23:48:18 +0200

    fix config

--- dialog_system/config.py
+++ dialog_system/config.py
@@ -20,23 +20,23 @@ class Config:
         quit_config = False
         while not quit_config:
             print(f"Current settings:\n"
-                  f"capslock:\t\t\t\t{self.caps_lock}\n"
-                  f"typochecker:\t\t\t{self.typo_check}\n"
-                  f"levenshtein distance:\t{self.levenshtein}\n"
-                  f"system delay:\t\t\t{self.system_delay} seconds\n"
-                  f"debug:\t\t\t\t\t{self.debug_mode}\n")
+                  f"capslock: {self.caps_lock}\n"
+                  f"typochecker: {self.typo_check}\n"
+                  f"levenshtein distance: {self.levenshtein}\n"
+                  f"system delay: {self.system_delay} seconds\n"
+                  f"debug: {self.debug_mode}\n")
             text = input("To change a setting, type \"[setting] [value]\". e.g. \"capslock True\"\nTo go back, "
                          "type \'return\':\n")
             splitinput = str(text).split()
             if splitinput[0] == "return":
                 quit_config = True
-            if splitinput[0] == "caps-lock":
+            if splitinput[0] == "capslock":
                 self.caps_lock = (splitinput[1].lower() == 'true')
-            if splitinput[0] == "typo-checker":
+            if splitinput[0] == "typochecker":
                 self.typo_check = (splitinput[1].lower() == 'true')
             if splitinput[0] == "levenshtein":
                 self.levenshtein = [int(i) for i in splitinput if i.isdigit()][0]
-            if splitinput[0] == "system-delay":
+            if splitinput[0] == "system":
                 self.system_delay = [float(i) for i in splitinput if i.replace('.', '', 1).isdigit()][0]
             if splitinput[0] == "debug":
                 self.debug_mode = (splitinput[1].lower() == 'true')
@@ -44,8 +44,8 @@ class Config:
 
 def create_config_parser():
     parser = argparse.ArgumentParser(description="Configure system settings.")
-    parser.add_argument("--caps-lock", type=bool, default=CAPS_LOCK, help="Enable or disable caps lock.")
-    parser.add_argument("--typo-check", type=bool, default=TYPO_CHECK, help="Enable or disable typo double-checking.")
+    parser.add_argument("--capslock", type=bool, default=CAPS_LOCK, help="Enable or disable caps lock.")
+    parser.add_argument("--typocheck", type=bool, default=TYPO_CHECK, help="Enable or disable typo double-checking.")
     parser.add_argument("--levenshtein", type=int, default=LEVENSHTEIN_DISTANCE, help="Set the Levenshtein distance for auto-correct.")
     parser.add_argument("--system-delay", type=float, default=SYSTEM_DELAY, help="Set the system output delay (in seconds).")
     parser.add_argument("--debug-mode", type=bool, default=DEBUG_MODE, help="Enable or disable debug mode.")
--- dialog_system/main.py
+++ dialog_system/main.py
@@ -1,6 +1,6 @@
+from classifiers.feedforward_nn import FeedForwardNN
 from data.data_processor import train_data
 from dialog_system.dialog_manager import DialogManager
-from classifiers.logistic_regression import LogisticRegressionModel
 from dialog_system.config import create_config_parser, Config
 from dialog_system.reasoning import handle_reasoning
 
@@ -10,15 +10,14 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     config = Config(
-        caps_lock=args.caps_lock,
-        typo_check=args.typo_check,
+        caps_lock=args.capslock,
+        typo_check=args.typocheck,
         levenshtein=args.levenshtein,
         system_delay=args.system_delay,
         debug_mode=args.debug_mode,
     )
 
-    # manager = DialogManager(FeedForwardNN(train_data, debug=DEBUG_MODE))
-    manager = DialogManager(LogisticRegressionModel(train_data), config)
+    manager = DialogManager(FeedForwardNN(train_data, debug=config.debug_mode), config)
     suggestions = manager.converse()
 
     if suggestions is not None:
