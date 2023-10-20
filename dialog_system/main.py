7dd639d808c80b8d66c77b05d3b28b0a0c6f47da
Yann <yannick.denboer@vangoghstraat.com>
2023-10-20 11:46:51 +0200

    p

---
+++ dialog_system/main.py
@@ -0,0 +1,28 @@
+import sys  
+sys.path.insert(1, r'C:\Users\yanni\Documents\GitHub\MAIR-Project-G28')
+
+from data.data_processor import train_data
+from dialog_system.dialog_manager import DialogManager
+from classifiers.logistic_regression import LogisticRegressionModel
+from dialog_system.config import create_config_parser, Config
+from dialog_system.reasoning import handle_reasoning
+
+
+if __name__ == "__main__":
+    parser = create_config_parser()
+    args = parser.parse_args()
+
+    config = Config(
+        caps_lock=args.caps_lock,
+        typo_check=args.typo_check,
+        levenshtein=args.levenshtein,
+        system_delay=args.system_delay,
+        debug_mode=args.debug_mode,
+    )
+
+    # manager = DialogManager(FeedForwardNN(train_data, debug=DEBUG_MODE))
+    manager = DialogManager(LogisticRegressionModel(train_data), config)
+    suggestions = manager.converse()
+
+    if suggestions is not None:
+        handle_reasoning(suggestions)
